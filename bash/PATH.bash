7d2be6560b19165f09e6f9c4dc5c14ded51475e5
Mike Schutte <schutte@quikly.com>
2018-01-10 09:26:41 -0500

    Add psql path

--- bash/PATH.bash
+++ bash/PATH.bash
@@ -4,4 +4,4 @@ export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH="$HOME/.nodenv/bin:$PATH"
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
-
+export PATH="/usr/local/opt/postgresql@9.5/bin:$PATH"
