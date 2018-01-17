c3a00a0ac109c9a05f53707d780d210b0134f510
Mike Schutte <schutte@quikly.com>
2018-01-17 10:52:27 -0600

    Add rbenv nodenv path

--- bash/PATH.bash
+++ bash/PATH.bash
@@ -3,6 +3,8 @@ export PATH="/usr/local/sbin:$PATH"
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
 export PATH="/usr/local/opt/postgresql@9.5/bin:$PATH"
+export PATH="$HOME/.nodenv/bin:$PATH"
+export PATH="$HOME/.rbenv/bin:$PATH"
 
 eval "$(rbenv init -)"
 eval "$(nodenv init -)"
