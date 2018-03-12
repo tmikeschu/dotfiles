1b32951bc8e7531e997986c54eb5d75790aee5fd
Mike Schutte <tmikeschutte@gmail.com>
2018-03-12 19:57:39 -0400

    Exclude node_modules from grepd

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -1,7 +1,7 @@
 alias rebash="source ~/.bash_profile"
 
 # shell commands
-alias grepd="grep -Rin --color"
+alias grepd="grep -Rin --exclude-dir node_modules --color"
 
 # Frequent Projects
 alias spoken="cd ~/projects/the-spoken-tour"
