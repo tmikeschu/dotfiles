3ef57c417b694a0148aeab8b9bafdbd41ae21507
Mike Schutte <schutte@clinc.com>
2018-12-16 15:16:52 -0500

    Lint/fix make_sym_links

--- make_sym_links.rb
+++ make_sym_links.rb
@@ -1,32 +1,29 @@
 #!/usr/bin/env ruby
 
-require 'fileutils'
-include FileUtils
+require "fileutils"
 
 begin
-  mkdir("~/.config")
-rescue 
+  FileUtil.mkdir("~/.config")
+rescue
   puts "~/.config already exists 😬\n\n"
 end
 
 links = {
-  "~/.bash_profile"        => "~/dotfiles/.bash_profile",
-  "~/.bashrc"              => "~/dotfiles/.bash_profile",
-  "~/.config/nvim"         => "~/.vim",
+  "~/.bash_profile" => "~/dotfiles/.bash_profile",
+  "~/.bashrc" => "~/dotfiles/.bash_profile",
+  "~/.config/nvim" => "~/.vim",
   "~/.git-completion.bash" => "~/dotfiles/.git-completion.bash",
-  "~/.gvimrc"              => "~/dotfiles/.vimrc",
-  "~/.inputrc"             => "~/dotfiles/.inputrc ",
-  "~/.set-defaults"        => "~/dotfiles/.set-defaults",
-  "~/.vim/ftplugin"        => "~/dotfiles/neovim/ftplugin",
-  "~/.vim/init.vim"        => "~/dotfiles/.vimrc",
-  "~/.vimrc"               => "~/dotfiles/.vimrc",
+  "~/.gvimrc" => "~/dotfiles/.vimrc",
+  "~/.inputrc" => "~/dotfiles/.inputrc ",
+  "~/.set-defaults" => "~/dotfiles/.set-defaults",
+  "~/.vim/ftplugin" => "~/dotfiles/neovim/ftplugin",
+  "~/.vim/init.vim" => "~/dotfiles/.vimrc",
+  "~/.vimrc" => "~/dotfiles/.vimrc",
 }
 
 links.each do |new, old|
-  begin
-    symlink(old, new)
-    puts "Symlink created for #{old} -> #{new} ✅"
-  rescue
-    puts "File at #{new} already exists 😬\n\n"
-  end
+  FileUtil.symlink(old, new)
+  puts "Symlink created for #{old} -> #{new} ✅"
+rescue
+  puts "File at #{new} already exists 😬\n\n"
 end
