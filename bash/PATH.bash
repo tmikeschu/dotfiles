1425dab98ceab057db89d055a408ef57da14217f
Mike Schutte <tmikeschutte@gmail.com>
2019-06-05 17:39:30 -0400

    Add {r,n}vm PATH shiz

--- bash/PATH.bash
+++ bash/PATH.bash
@@ -14,6 +14,9 @@ export PATH="$HOME/.linuxbrew/bin:$PATH"
 export PATH="$HOME/.jenv/bin:$PATH"
 export PATH="/home/ubuntu/.linuxbrew/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+export NVM_DIR="$HOME/.nvm"
 
 if hash rbenv 2>/dev/null
 then
@@ -29,3 +32,13 @@ if hash jenv 2>/dev/null
 then
   eval "$(jenv init -)"
 fi
+
+if hash rvm 2>/dev/null
+then
+  # Load RVM into a shell session *as a function*
+  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+fi
+
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
