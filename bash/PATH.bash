dcc9ef996de4f9bc5158c4fed29d3c291b3a04be
Mike Schutte <tmikeschutte@gmail.com>
2019-06-10 10:27:05 -0400

    Fix rvm load

--- bash/PATH.bash
+++ bash/PATH.bash
@@ -32,12 +32,6 @@ then
   eval "$(jenv init -)"
 fi
 
-if hash rvm 2>/dev/null
-then
-  # Load RVM into a shell session *as a function*
-  [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-fi
-
-
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
