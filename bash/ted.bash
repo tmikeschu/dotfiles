cd103412f50dfbf41cb6f410b314409b6f796fb1
Mike Schutte <tmikeschutte@gmail.com>
2020-02-06 10:27:58 -0500

    Add more ted aliases

--- bash/ted.bash
+++ bash/ted.bash
@@ -1,4 +1,10 @@
 export PATH="$PATH:${HOME}/code/ted/infrastructure/bin"
 
 alias rvminstall='rvm install `cat .ruby-version`'
-alias ted='cd code/ted'
+alias ted='cd ~/code/ted'
+alias fomo='cd ~/code/ted/fomoconf'
+alias twt='cd ~/code/ted/twt-checkin'
+alias ems='cd ~/code/ted/ems'
+alias concierge='cd ~/code/ted/concierge'
+alias idea='cd ~/code/ted/tedster-lounge'
+alias pwall='cd ~/code/ted/program-wall'
