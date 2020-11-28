bfd339269a60e6489c7b5947c8a701c8ffdb8e10
Mike Schutte <tmikeschutte@gmail.com>
2020-11-28 11:20:36 -0500

    better symlinks formatting

--- make_symlinks.js
+++ make_symlinks.js
@@ -8,27 +8,29 @@ const log = {
   info: (...msgs) => console.info(chalk.black.bgBlueBright(...msgs)),
 };
 
-const main = () => {
-  log.info("Making symlinks");
+/**
+ * @returns string
+ */
+const stripUser = (path) =>
+  path.replace("/Users/tmikeschutte/", "").slice(-30).padStart(30, " ");
 
-  existing = [];
+const main = () => {
+  log.info("Making symlinks\n");
+
   Object.entries(symlinks).forEach(([symbol, source]) => {
     try {
       fs.symlinkSync(source, symbol, "file");
-      log.success(`Created link from ${source} to ${symbol}`);
+      log.success(`${stripUser(symbol)} -> ${stripUser(source)}`);
     } catch (err) {
       if (err.message.includes("EEXIST")) {
-        existing.push(symbol.replace("/Users/tmikeschutte/", ""));
+        log.info(`exists: ${stripUser(symbol)} -> ${stripUser(source)}`);
         return;
       }
       log.error(err.message);
     }
   });
 
-  if (existing.length > 0) {
-    log.info(`\nLinks already existed for: ${existing.join("\n")}\n`);
-  }
-  log.info("All done.");
+  log.info("\nAll done.");
 };
 
 main();
