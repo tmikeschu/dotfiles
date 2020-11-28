996a8cb2a74c4ea18efa8d04cc7f7540ea715b89
Mike Schutte <tmikeschutte@gmail.com>
2020-11-28 10:17:17 -0500

    better symlinks script

--- make_symlinks.js
+++ make_symlinks.js
@@ -3,21 +3,31 @@ const symlinks = require("./symlinks.json");
 const chalk = require("chalk");
 
 const log = {
-  error: (...msgs) => console.error(chalk.black.bold.bgRedBright(...msgs)),
-  success: (...msgs) => console.log(chalk.black.bold.bgGreenBright(...msgs)),
-  info: (...msgs) => console.info(chalk.black.bold.bgGreenBright(...msgs)),
+  error: (...msgs) => console.error(chalk.black.bgRedBright(...msgs)),
+  success: (...msgs) => console.log(chalk.black.bgGreenBright(...msgs)),
+  info: (...msgs) => console.info(chalk.black.bgBlueBright(...msgs)),
 };
 
 const main = () => {
-  log.info("Creating symlinks");
-  Object.entries(symlinks).forEach(([link, symbol]) => {
+  log.info("Making symlinks");
+
+  existing = [];
+  Object.entries(symlinks).forEach(([symbol, source]) => {
     try {
-      fs.symlinkSync(symbol, link, "file");
-      log.success(`Created link from ${symbol} to ${link}`);
+      fs.symlinkSync(source, symbol, "file");
+      log.success(`Created link from ${source} to ${symbol}`);
     } catch (err) {
-      log.error(err.toString());
+      if (err.message.includes("EEXIST")) {
+        existing.push(symbol.replace("/Users/tmikeschutte/", ""));
+        return;
+      }
+      log.error(err.message);
     }
   });
+
+  if (existing.length > 0) {
+    log.info(`\nLinks already existed for: ${existing.join("\n")}\n`);
+  }
   log.info("All done.");
 };
 
